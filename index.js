cf8f8023745810946b6d7ade7b527d1733629897
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-02-16 20:12:53 +0530

    create file with user input

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-//use this link - https://Nodejs-practice.chandrashekha42.repl.co
+const fs = require('fs');
 
-console.log(process.argv[2]); // it will give array of 2 elements
-
+const input = process.argv;
+fs.writeFileSync(input[2], input[3]); 
+// command - node index.js apple.txt "this is apple"
