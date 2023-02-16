552244bd488bf737d9cf41b34d51b66a2a6c7360
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-02-16 16:10:08 +0530

    how to use http module

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
-const fs = require('fs');
+//const fs = require('fs');
+const http = require('http');
+
+http.createServer((req, resp) => {
+  resp.write("test hhtp module");
+  resp.end();
+}).listen(4500);
