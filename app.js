2431027deb09efac0f48ef9bb7ee3656323332f6
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-02-16 14:53:33 +0530

    How to create file

--- app.js
+++
@@ -1,9 +0,0 @@
-// wrong way
-// export let x = 10;
-// export let y = 20;
-
-//right way
-module.exports = {
-  x:10,
-  y:20
-}
--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// run `node index.js` in the terminal
 
-const app = require('./app');
-//console.log(app.x);
-
-//console.log(`Hello Node.js v${process.versions.node}!`);
-
-const arr = [2,4,7,1,3,8,3];
-
-let result = arr.filter((item) => {
-  return item >= 4;
-})
-
-console.log(result);
-
+const fs = require('fs');
+fs.writeFileSync("hello.txt", "first file");
