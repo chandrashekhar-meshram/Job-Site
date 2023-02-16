f7bad767d209285ad7a0c95fe87256efcc0f489f
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-02-16 14:15:58 +0530

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 // run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const app = require('./app');
+//console.log(app.x);
+
+//console.log(`Hello Node.js v${process.versions.node}!`);
+
+const arr = [2,4,7,1,3,8,3];
+
+let result = arr.filter((item) => {
+  return item >= 4;
+})
+
+console.log(result);
+
