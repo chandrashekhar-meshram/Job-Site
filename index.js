c7c672e90cc5336b41262c57f271fda3e425be49
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-02-16 19:42:27 +0530

    api created

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hi bolo');
+const http = require('http');
+http.createServer((req, resp) => {
+  resp.writeHead(200, { 'Content-Type': 'application\json' });
+  resp.write(JSON.stringify({ name: 'Shekhar', email: 'abc@gmail.com' }));
+  resp.end();
+}).listen(5000);
+
+
+//use this link - https://Nodejs-practice.chandrashekha42.repl.co
+
