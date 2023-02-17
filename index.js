0ca1c051eca8d8487ee1cda96f72d80209f6b3ec
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-02-17 16:41:49 +0530

    read file

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const path = require('path');
 const dirPath = path.join(__dirname, 'crud');
 const filePath = `${dirPath}/apple.txt`;
 
-fs.writeFileSync(filePath, 'this is curd file');
+//fs.writeFileSync(filePath, 'this is curd file');
+
+fs.readFile(filePath, 'utf8', (err, item) => {
+  console.log(item);
+});
+
+//o/p - htis is curd file
