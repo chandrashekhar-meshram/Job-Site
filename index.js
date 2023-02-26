d8fc05e0fb9c8ee498985f95105426d3f30d8914
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-02-26 18:27:48 +0530

    connected to cloud db using VS-code

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const app = express();
+
 const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', true);
+
+mongoose
+  .connect(
+    'mongodb+srv://shekharonlineproject:shekharonlineproject@cluster0.8rwhj8f.mongodb.net/tutorial?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.warn('db connected');
+  })
+.catch((e) => {
+  console.log('DB error ', e);
+});
