ab2387560eb3807dcd9f394c64dbba4fabd50ab2
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-02-27 17:36:13 +0530

    get api

--- index.js
+++ index.js
@@ -8,24 +8,18 @@ const User = require('./models/users');
 mongoose.set('strictQuery', false);
 mongoose.set('strictQuery', true);
 
-mongoose
-  .connect(
-    'mongodb+srv://shekharonlineproject:shekharonlineproject@cluster0.8rwhj8f.mongodb.net/tutorial?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  );
+mongoose.connect(
+  'mongodb+srv://shekharonlineproject:shekharonlineproject@cluster0.8rwhj8f.mongodb.net/tutorial?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
-  const data = new User({
-    _id:mongoose.Types.ObjectId(),
-    name:'tony',
-    email:'tony@gmail.com'
-  })
-
-  data.save().then((result) => {
-    console.log(result);
-  })
-  .catch((err) => {
-    console.log(err);
+app.get('/users', function (req, res) {
+  User.find().then((data) => {
+    res.json(data);
   });
+});
+
+app.listen(4000);
